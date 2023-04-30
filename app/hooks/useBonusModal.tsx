787d39d575cd69ad6e84ffd4f7fe1111f2995b78
usman746 <usman.nasir.387@gmail.com>
2023-05-01 02:56:29 +0500

    signup modal

--- app/hooks/useBonusModal.tsx
+++ app/hooks/useBonusModal.tsx
@@ -49,18 +49,21 @@ export const useBonusModal = (): [React.ReactNode, () => void] => {
         scrollAreaComponent={ScrollArea.Autosize}
         styles={{
           content: {
-            backgroundColor: '#F2F2F2',
+            backgroundColor: '#01152D',
           },
           header: {
-            backgroundColor: '#F2F2F2',
+            backgroundColor: '#01152D',
           },
           close: {
             position: 'absolute',
             top: 30,
             right: 30,
             borderRadius: '20px',
-            color: 'black',
-            backgroundColor: 'white',
+            color: '#016BE6',
+            backgroundColor: 'transparent',
+            '&:hover': {
+              backgroundColor: 'transparent',
+            },
           },
         }}
         opened={opened}
@@ -76,13 +79,13 @@ export const useBonusModal = (): [React.ReactNode, () => void] => {
           <Grid align="top">
             <Grid.Col md={9}>
               <Stack align="flex-start" my={20}>
-                <Title order={2} size={'33px'} color="black" className=" font-bold uppercase">
+                <Title order={2} size={'33px'} color="#FFFFFF" className=" font-bold uppercase">
                   AHOY THERE MATEY!
                 </Title>
                 <Text size="md" color="#6D7D8E">
                   Ready to ride the Waves of Good Fortune with us on our high seas adventure?
                 </Text>
-                <Text size="sm" className="font-normal" color="#0F0F0F">
+                <Text size="sm" className="font-normal" color="#FFFFFF">
                   But first, we need to know a bit about you. Don't worry, your data is safe with us. We'll protect it
                   like we protect our precious loot. So, come aboard and let's set sail to treasure-filled waters!
                 </Text>
@@ -93,13 +96,13 @@ export const useBonusModal = (): [React.ReactNode, () => void] => {
                     <Button
                       styles={{
                         root: {
-                          fontSize: '16px',
+                          fontSize: '10px',
                           color: 'white',
-                          '&:hover': { background: '#751F86' },
+                          '&:hover': { background: '#016BE6' },
                         },
                       }}
-                      bg={'#751F86'}
-                      radius={0}
+                      bg={'#016BE6'}
+                      radius={50}
                       px={40}
                       className="text-base font-light"
                       type="submit"
@@ -107,18 +110,15 @@ export const useBonusModal = (): [React.ReactNode, () => void] => {
                     >
                       UNLOCK MY BONUS
                     </Button>
-                    <Text className="underline text-base font-bold cursor-pointer" onClick={close}>
+                    <Text color={'white'} className="underline text-base font-bold cursor-pointer" onClick={close}>
                       No thanks, I'll play without it.
                     </Text>
                   </Group>
                 </form>
                 
                 <Group>
-                  <Text size={'sm'} component={NextLink} href="/terms-and-conditions" color="dark">
-                    Terms and Conditions
-                  </Text>
-                  <Text size={'sm'} component={NextLink} href="/privacy-policy" color="dark">
-                    Privacy Policy
+                  <Text size={'sm'} component={NextLink} href="/terms-and-conditions" color="white">
+                  Aye, Aye your code will be sent to your email address.
                   </Text>
                 </Group>
               </Stack>
@@ -138,13 +138,14 @@ export const useBonusModal = (): [React.ReactNode, () => void] => {
 
 const useStyles = createStyles((theme) => ({
   input: {
-    borderRadius: 3,
-    border: '1px solid #84848470 !important',
+    borderRadius: 10,
+    border: '1px solid #016BE6 !important',
     height: '60px',
     width: '100%',
-    backgroundColor: '#F2F2F2',
+    color:'white',
+    backgroundColor: 'transparent',
     '::placeholder': {
-      color: '#393939',
+      color: '#ffffff',
     },
   },
 }));
