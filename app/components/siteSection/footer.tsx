532de890f79f1218b05ae49271367001b94c241e
Saqib Javed Qureshi <saqib743@live.com>
2023-05-06 18:17:21 +0500

    Footer Fixed

--- app/components/siteSection/footer.tsx
+++ app/components/siteSection/footer.tsx
@@ -69,30 +69,100 @@ export function Footer() {
       });
   };
   return (
-    <div className={classes.backdrop}>
-      <Container pos="relative" py={50} size={1300}>
+    <div
+      className={classes.backdrop}
+      style={{ zIndex: 50, boxShadow: '-1px 0px 20px 3px #016BE6', borderTop: '6px solid #016BE6' }}
+    >
+      <Container pos="relative" py={50} size={1300} style={{ marginTop: '50px' }}>
         <Grid align="top">
           <Grid.Col sm={12} className="text-center">
             <div className="text-center">
               <Group position="center">
-                <Image src="/images/footer/footer2.png" width={63} height={62} alt="JACOG" />
-                <Image src="/images/footer/footer3.png" width={70} height={70} alt="JACOG" />
-                <Image src="/images/footer/footer4.png" width={58} height={58} alt="JACOG" />
-                <Image src="/images/footer/footer5.png" width={76} height={76} alt="JACOG" />
-                <Image src="/images/footer/footer1.png" width={73} height={66} alt="JACOG" />
+                <Image
+                  src="/images/footer/footer2.png"
+                  width={63}
+                  height={62}
+                  alt="JACOG"
+                  style={{ marginRight: '10px' }}
+                />
+                <Image
+                  src="/images/footer/footer3.png"
+                  width={70}
+                  height={70}
+                  alt="JACOG"
+                  style={{ marginRight: '10px' }}
+                />
+                <Image
+                  src="/images/footer/footer4.png"
+                  width={58}
+                  height={58}
+                  alt="JACOG"
+                  style={{ marginRight: '10px' }}
+                />
+                <Image
+                  src="/images/footer/footer5.png"
+                  width={76}
+                  height={76}
+                  alt="JACOG"
+                  style={{ marginRight: '10px' }}
+                />
+                <Image
+                  src="/images/footer/footer1.png"
+                  width={73}
+                  height={66}
+                  alt="JACOG"
+                  style={{ marginRight: '10px' }}
+                />
               </Group>
               <Divider className="my-5 mx-auto" w={50} size={'sm'} orientation="horizontal" />
               <Group mt={30} position="center">
-                <Image src="/images/footer/payment1.png" width={42} height={41} alt="JACOG" />
-                <Image src="/images/footer/payment2.png" width={50} height={31.5} alt="JACOG" />
-                <Image src="/images/footer/payment3.png" width={59} height={37} alt="JACOG" />
-                <Image src="/images/footer/payment4.png" width={96} height={41} alt="JACOG" />
+                <Image
+                  src="/images/footer/payment1.png"
+                  width={42}
+                  height={41}
+                  alt="JACOG"
+                  style={{ marginRight: '10px' }}
+                />
+                <Image
+                  src="/images/footer/payment2.png"
+                  width={50}
+                  height={31.5}
+                  alt="JACOG"
+                  style={{ marginRight: '10px' }}
+                />
+                <Image
+                  src="/images/footer/payment3.png"
+                  width={59}
+                  height={37}
+                  alt="JACOG"
+                  style={{ marginRight: '10px' }}
+                />
+                <Image
+                  src="/images/footer/payment4.png"
+                  width={96}
+                  height={41}
+                  alt="JACOG"
+                  style={{ marginRight: '10px' }}
+                />
               </Group>
               <Divider className="my-5 mx-auto" w={50} size={'sm'} orientation="horizontal" />
               <Group mt={30} className="sm:space-x-4 xs:space-x-0" position="center">
-                <BrandApple fill="#CAC6CB" color="#CAC6CB" />
-                <BrandWindows color="#CAC6CB" />
+                <Image
+                  src="/images/footer/apple.png"
+                  width={19.29}
+                  height={22.89}
+                  alt="JACOG"
+                  style={{ marginRight: '10px' }}
+                />
+                <Image
+                  src="/images/footer/windows.png"
+                  width={19.29}
+                  height={22.89}
+                  alt="JACOG"
+                  style={{ marginRight: '10px' }}
+                />
                 <BrandAndroid color="#CAC6CB" />
+
                 <Divider orientation="vertical" />
 
                 <DeviceDesktop color="#CAC6CB" />
@@ -101,7 +171,6 @@ export function Footer() {
               </Group>
 
               <Button
-                radius={50}
                 className="w-[45%] lg:w-auto mt-5"
                 // styles={{ label: { fontSize: '16px', fontWeight: 'lighter' } }}
                 styles={{
@@ -138,27 +207,47 @@ export function Footer() {
                 </ActionIcon>
               </Group>
 
-              <Text mt={30} color={'#ACACAC'} className="font-[300]" size="sm">
+              <Text mt={30} color={'#ACACAC'} className="font-[400]" size="sm" style={{ fontSize: '12px' }}>
                 We care about protecting your data. Read more in our <br />{' '}
-                <Text color={'#ffffff'} size="sm" component={NextLink} href="/privacy-policy" className="font-[700]">
+                <Text
+                  color={'#ffffff'}
+                  size="sm"
+                  component={NextLink}
+                  href="/privacy-policy"
+                  className="font-[400]"
+                  style={{ fontSize: '12px' }}
+                >
                   Privacy Policy.
                 </Text>
               </Text>
             </div>
           </Grid.Col>
+        </Grid>
+      </Container>
+      <div
+        style={{
+          height: '1px',
+          background: 'linear-gradient(to left,  #FFFFFF,  #212121)',
+          backgroundClip: 'padding-box',
+          width: '100%',
+        }}
+      />
+
+      <Container pos="relative" py={50} size={1300}>
+        <Grid align="top">
           <Grid.Col xs={12}>
-            <Divider className="mt-10" size={'sm'} orientation="horizontal" />
-            <div className="text-center space-y-2 mt-10">
+            <div className="text-center space-y-2">
               <Text
                 color={'#ffffff'}
                 size="md"
-                className="block font-[500]"
+                className="block font-[400]"
                 component={NextLink}
                 href="/terms-and-conditions"
+                style={{ fontSize: '18px' }}
               >
                 Terms and Conditions
               </Text>
-              <Text color={'#ACACAC'} size="sm">
+              <Text color={'#ACACAC'} size="sm" style={{ fontSize: '16px', marginTop: '15px' }}>
                 © Copyright {new Date().getFullYear()} Pasa Slots All Rights Reserved
               </Text>
             </div>
