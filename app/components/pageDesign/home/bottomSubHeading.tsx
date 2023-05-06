82a1bedb106140573e8c626fab59c7814bd566d4
Saqib Javed Qureshi <saqib743@live.com>
2023-05-06 18:25:13 +0500

    second last section fixed

--- app/components/pageDesign/home/bottomSubHeading.tsx
+++ app/components/pageDesign/home/bottomSubHeading.tsx
@@ -3,22 +3,44 @@ import { NextLink } from '@mantine/next';
 
 export const BottomSubHeading = () => {
   return (
-    <div className="bg-gradient-to-r from-blue-500 to-purple-600"
-    style={{
-      backgroundColor: '#150B2E',
-      backgroundImage: 'radial-gradient(at 20% 25%, hsla(212,98%,45%,0.4) 0px, transparent 50%), radial-gradient(at 78% 26%, hsla(287,50%,40%,0.78) 0px, transparent 50%), radial-gradient(at 51% 68%, hsla(308,37%,47%,0.57) 0px, transparent 50%)',
-      backdropFilter: 'blur(20px)',
-      backgroundSize: 'cover',
-    }}>
+    <div
+      className="bg-gradient-to-r from-blue-500 to-purple-600"
+      style={{
+        backgroundColor: '#150B2E',
+        backgroundImage:
+          'radial-gradient(at 20% 25%, hsla(212,98%,45%,0.4) 0px, transparent 50%), radial-gradient(at 78% 26%, hsla(287,50%,40%,0.78) 0px, transparent 50%), radial-gradient(at 51% 68%, hsla(308,37%,47%,0.57) 0px, transparent 50%)',
+        backdropFilter: 'blur(20px)',
+        backgroundSize: 'cover',
+        zIndex: 50,
+        boxShadow: '-1px 0px 20px 3px #016BE6',
+        borderTop: '6px solid #016BE6',
+      }}
+    >
       <Container fluid ml={90} mr={69} py={50}>
         <Grid align="top">
           <Grid.Col sm={12}>
-            <Image className='mx-auto mb-10' width={508} height={545} src="/images/pages/home/cardsfalling.png" alt='image' />
+            <Image
+              className="mx-auto mb-10"
+              width={508}
+              height={545}
+              src="/images/pages/home/cardsfalling.png"
+              alt="image"
+            />
             <div className="space-y-5 text-center">
               <Title order={2} className="font-bold uppercase text-[#FFFFFF]">
                 Sun, Sand, and Big Wins<br></br> Await You
               </Title>
-              <Text className='text-center text-[#FFFFFF] opacity-70' size={'sm'}>
+              <Text
+                className="text-center text-[#FFFFFF] opacity-80"
+                size={'sm'}
+                style={{
+                  fontWeight: '400',
+                  fontSize: '20px',
+                  marginTop: '30px',
+                  lineHeight: '37.74px',
+                  letterSpacing: '-1.5%',
+                }}
+              >
                 Begin your adventure of gaming with Pasa Slots today.
               </Text>
             </div>
@@ -37,8 +59,9 @@ export const BottomSubHeading = () => {
                 }}
                 bg={'linear-gradient(to bottom, #2072D2, #A74C9A)'}
                 radius={50}
-                component='a'
+                component="a"
                 href="http://Pasasweeps.net"
+                style={{ marginTop: '50px', border: 'none' }}
               >
                 PLAY NOW
               </Button>
