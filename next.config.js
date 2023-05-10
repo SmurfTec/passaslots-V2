7083ed5412147250ae5ee3faa5a14ff3efa71f60
Saqib Javed Qureshi <saqib.javed@turingtechnologies.org>
2023-05-11 00:40:54 +0500

    Integrated cloudinary in POST API

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   env: {
     BASE_URL: process.env.BASE_URL || 'localhost:4000',
     BLOGS_URL: process.env.BLOGS_URL || '/blogs',
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
   },
   serverRuntimeConfig: {
     PROJECT_ROOT: __dirname,
