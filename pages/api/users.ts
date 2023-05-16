7453ba044a2aedee2a9414dcaba65363f9986f24
Saqib Javed Qureshi <saqib743@live.com>
2023-05-16 19:42:11 +0500

    Fixed API new

---
+++ pages/api/users.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from '@prisma/client';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import NextCors from 'nextjs-cors';
+
+const prisma = new PrismaClient();
+
+export default async function ContactHandler(req: NextApiRequest, res: NextApiResponse<any>) {
+  const { method, body } = req;
+  switch (method) {
+    case 'GET':
+      try {
+        await NextCors(req, res, {
+          methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+          origin: '*',
+          optionsSuccessStatus: 200,
+        });
+        const users = await prisma.playerRegistration.findMany();
+        res.status(200).json(users);
+      } catch (err) {
+        res.status(400).json({ message: `Something went wrong! Please read the error message '${err}'` });
+      }
+      break;
+  }
+}
