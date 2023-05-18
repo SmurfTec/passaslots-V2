3fa0b177dc5d30276421a201c8332ce92d7a8ff3
Saqib Javed Qureshi <saqib743@live.com>
2023-05-18 17:55:30 +0500

    API fixes

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -19,6 +19,7 @@ export default async function ContactHandler(req: NextApiRequest, res: NextApiRe
             email: body.email,
             name: body.name,
             phone: body.phone,
+            unlockMyBonusCheck: body.unlockMyBonusCheck,
           },
         });
         res.status(200).json(contact);
